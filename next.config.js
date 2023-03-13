014e87f70a1f3cc328b766600a81d8dc9a89a558
logicalOgbonna <arinnzeogbo@gmail.com>
2023-03-13 14:35:02 +0100

    update: removed optimization from generation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  console.log("🚀 ~ file: next.config.js:11 ~ repo:", repo)
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
 }
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true
+  }
+};
+
+module.exports = nextConfig;
