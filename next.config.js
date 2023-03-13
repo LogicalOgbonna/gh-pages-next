0216fbf47c946e9ec5c1fa7a614c0a6c32072e73
logicalOgbonna <arinnzeogbo@gmail.com>
2023-03-13 14:52:57 +0100

    update: removed a lot in next config

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  console.log("🚀 ~ file: next.config.js:11 ~ repo:", repo)
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   images: {
     unoptimized: true
   }
